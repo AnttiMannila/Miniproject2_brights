32c26920a235ed6f47be1306726a869755973766
Carl Ilkka <160502085+IlkkaCarl@users.noreply.github.com>
2024-03-13 11:35:39 +0200

    Update work.py
    Added functions to check for nulls, valuetypes and to fix valuetypes

--- work.py
+++ work.py
@@ -25,14 +25,36 @@ def join_dataframes(solar_df, temp_snow_df, cloud_coverage_df, venue):
     return merged_df
 
 def count_nulls():
-    finished_csvs = os.listdir(os.path.join(os.getcwd(), "Finished_CSVs"))
+    finished_csvs = os.listdir(os.path.join(os.getcwd(), "Fixed_CSVs"))
     for i in finished_csvs:
-        file = os.path.join(os.getcwd(), "Finished_CSVs", i)
+        file = os.path.join(os.getcwd(), "Fixed_CSVs", i)
         df = pd.read_csv(file)
         print()
         print(file)
         print(df.isnull().sum())
         print(len(df))
+
+def check_value_types():
+    finished_csvs = os.listdir(os.path.join(os.getcwd(), "Fixed_CSVs"))
+    for i in finished_csvs:
+        file = os.path.join(os.getcwd(), "Fixed_CSVs", i)
+        df = pd.read_csv(file)
+        print()
+        print(file)
+        print(df.dtypes)
+
+def fix_value_types():
+    finished_csvs = os.listdir(os.path.join(os.getcwd(), "Finished_CSVs"))
+    for file in finished_csvs:
+        i = os.path.join(os.getcwd(), "Finished_CSVs", file)
+        df = pd.read_csv(i)
+        df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d', errors='coerce')
+        df = df.sort_values('Date')
+        df['Average temperature [°C]'] = pd.to_numeric(df['Average temperature [°C]'], errors='coerce')
+        df['Snow depth [cm]'] = pd.to_numeric(df['Snow depth [cm]'], errors='coerce') 
+        df['Cloud cover [1/8]'] = pd.to_numeric(df['Cloud cover [1/8]'], errors='coerce') 
+        df['Direct solar radiation mean [W/m2]'] = pd.to_numeric(df['Direct solar radiation mean [W/m2]'], errors='coerce') 
+        df.to_csv(os.path.join(os.getcwd(), "Fixed_CSVs", f"fixed_{file}"), index=False)
 
 # print(os.getcwd())
 
